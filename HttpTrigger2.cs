a9ddae21d1cad044252bc63359f5d0c23917537f
Akash <patilakash463@gmail.com>
2024-12-18 13:04:42 +0000

    trigger

--- HttpTrigger2.cs
+++ HttpTrigger2.cs
@@ -17,7 +17,7 @@ namespace Company.Function
         [Function("HttpTrigger2")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("test 2nd trigger");
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
